5787d01c58b040563ac64a1a736c92004e627963
Sena Kokoz Pazar <senakokoz@gmail.com>
2024-10-07 18:43:17 +0300

    s15d3 bitti

---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,42 @@
+package org.example.entity;
+
+public class Employee {
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public Employee(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Employee employee = (Employee) object;
+        return id == employee.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
